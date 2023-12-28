5916c4e97145781b3a9e540d2f63d60eda7586ea
Vasilica Petcu <vpetcu1@gmail.com>
2023-12-28 17:24:04 +0000

    windows changes

--- run.bat
+++ run.bat
@@ -1,19 +1,22 @@
-operation=%1
-IF "%operation%"=="start" (
-    call env.bat  
+@echo off
+set operation=%1
+call env.bat
+IF "%operation%" == "start" (
     if not exist %PROJECT%\ (
         mkdir %PROJECT%
     )
     if not exist home\ (
         mkdir home
     )
-    copy .profile home/.profile
-    copy -R .config/ home/
-    copy -R .init/ home/
-    set COMPOSE_PROJECT_NAME=${PROJECT} && docker-compose up --remove-orphans --force-recreate &
-) ELSE IF "%operation%"=="stop" (
-	docker-compose down
+    copy .profile home\.profile
+    robocopy .config home\.config /e
+    robocopy .init home\.init /e
+    set COMPOSE_PROJECT_NAME=${PROJECT} && docker-compose up --remove-orphans --force-recreate
 ) ELSE (
-    echo "Must pass one of the args start|stop"
+	IF "%operation%" == "stop" (
+	    docker-compose down
+	) ELSE (
+	    echo "Must pass one of the args start|stop"
+	)
 )
 
