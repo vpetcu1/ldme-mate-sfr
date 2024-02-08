6a8c5204d916331f39cb50da6ca8f8b1cd86925f
Vasilica Petcu <vpetcu1@gmail.com>
2024-02-08 16:59:11 +0000

    add some more tunnels

--- .init/tunnels.sh
+++ .init/tunnels.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 create_tunnels () {
 	echo ------------ OPENNING TUNELS
-	echo ------------ OPENNING MySQL on 3606 localhost
+	echo ------------ OPENNING HR MySQL on 3606 localhost
 	sshpass -p "$password" ssh -Nf -4 -L 3606:vi-mcc-mydis-201:3408 "$username"@sp-mut-adm-201.adm.gnp.les800
-	echo ------------ OPENNING MySQL on 3607 localhost
-	sshpass -p "$password" ssh -Nf -4 -L 3607:vi-mcc-myar-201:3418 "$username"@sp-mut-adm-201.adm.gnp.les800
+    echo ------------ OPENNING TRP MySQL on 3609 localhost
+	sshpass -p "$password" ssh -Nf -4 -L 3607:vi-mcc-mydis-301:3408 "$username"@sp-mut-adm-201.adm.gnp.les800
+	echo ------------ OPENNING CA MySQL on 3608 localhost
+	sshpass -p "$password" ssh -Nf -4 -L 3608:vi-mcc-myca-201:3406 "$username"@sp-mut-adm-201.adm.gnp.les800
+	echo ------------ OPENNING AR MySQL on 3607 localhost
+	sshpass -p "$password" ssh -Nf -4 -L 3609:vi-mcc-myar-201:3418 "$username"@sp-mut-adm-201.adm.gnp.les800
 	echo ------------ OPENNING Oracle on 1521 localhost
 	sshpass -p "$password" ssh -Nf -L1521:sv-mut-ora-202.int.gnp.les800:1521 "$username"@vi-mcc-dmc-201.adm.gnp.les800
 	echo ------------ OPENNING Oracle on 1522 localhost
