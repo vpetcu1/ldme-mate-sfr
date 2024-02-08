92fec4542bf041b4fbe741af1e8b3d53c291bd7f
Vasilica Petcu <vpetcu1@gmail.com>
2024-02-08 17:25:13 +0000

    add TRP AR

--- .init/tunnels.sh
+++ .init/tunnels.sh
@@ -7,8 +7,10 @@ create_tunnels () {
 	sshpass -p "$password" ssh -Nf -4 -L 3607:vi-mcc-mydis-301:3408 "$username"@sp-mut-adm-201.adm.gnp.les800
 	echo ------------ OPENNING CA MySQL on 3608 localhost
 	sshpass -p "$password" ssh -Nf -4 -L 3608:vi-mcc-myca-201:3406 "$username"@sp-mut-adm-201.adm.gnp.les800
-	echo ------------ OPENNING AR MySQL on 3607 localhost
+	echo ------------ OPENNING HR AR MySQL on 3609 localhost
 	sshpass -p "$password" ssh -Nf -4 -L 3609:vi-mcc-myar-201:3418 "$username"@sp-mut-adm-201.adm.gnp.les800
+	echo ------------ OPENNING TRP AR MySQL on 3610 localhost
+	sshpass -p "$password" ssh -Nf -4 -L 3610:vi-mcc-myar-301:3418 "$username"@sp-mut-adm-201.adm.gnp.les800
 	echo ------------ OPENNING Oracle on 1521 localhost
 	sshpass -p "$password" ssh -Nf -L1521:sv-mut-ora-202.int.gnp.les800:1521 "$username"@vi-mcc-dmc-201.adm.gnp.les800
 	echo ------------ OPENNING Oracle on 1522 localhost
